67d9215f7feed8d85ea2fa4026c6b3be06423f06
Tayyab Hussain <tayyabhussain@Tayyabs-MacBook-Pro.local>
2022-04-27 13:04:42 -0400

    admin functionality

--- backend/contracts/KToken.sol
+++ backend/contracts/KToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KToken is ERC20 {
-    constructor(address _treasuryAddress, uint256 _initialSupply) public ERC20("SimpToken", "SIMP") {
+    constructor(address _treasuryAddress, uint256 _initialSupply) public ERC20("Treasury Token", "TREASURE") {
         _mint(_treasuryAddress, _initialSupply * (10 **18));
     }
 }
